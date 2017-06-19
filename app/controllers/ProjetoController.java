bf431bbf1e3bb9bae2b41892de08081710e08f66
brunoluizdesiqueira <brunoluizdesiqueira@gmail.com>
2017-06-19 19:22:32 -0300

    Mudanças nos formularios de login, nova tarefa e projeto

--- app/controllers/ProjetoController.java
+++ app/controllers/ProjetoController.java
@@ -1,10 +1,12 @@
 package controllers;
 
+
 import daos.ClienteDAO;
 import models.Cliente;
 import models.Projeto;
 import play.data.Form;
 import play.data.FormFactory;
+import play.db.ebean.Transactional;
 import play.mvc.Controller;
 import play.mvc.Result;
 import validadores.ValidadorDeProjeto;
@@ -13,14 +15,15 @@ import views.html.formularioDeNovoProjeto;
 import javax.inject.Inject;
 import java.util.List;
 import java.util.Optional;
+
 public class ProjetoController extends Controller {
-    
+
     @Inject
     private FormFactory formularios;
     @Inject
     private ValidadorDeProjeto validadorDeProjeto;
     @Inject
-	private ClienteDAO clienteDAO;
+    private ClienteDAO clienteDAO;
 
     public Result formularioDeNovoProjeto() {
         Form<Projeto> formulario = formularios.form(Projeto.class);
@@ -28,31 +31,32 @@ public class ProjetoController extends Controller {
         return ok(formularioDeNovoProjeto.render(formulario, clientes));
     }
 
+    @Transactional
     public Result salvaNovoProjeto() {
         Form<Projeto> formulario = formularios.form(Projeto.class).bindFromRequest();
         List<Cliente> clientes = clienteDAO.todos();
         Projeto projeto = formulario.get();
+
         if (validadorDeProjeto.temErros(formulario)) {
-            flash("danger", "Há erros no formulário de cadastro de projeto!");			
-			return badRequest(formularioDeNovoProjeto.render(formulario, clientes));
-        }  
+            flash("danger", "Há erros no formulário de cadastro de projeto!");
+            return badRequest(formularioDeNovoProjeto.render(formulario, clientes));
+        }
 
         String cliente_id = formulario.field("cliente_id").valueOr("");
-        
+
         if (cliente_id.isEmpty()) {
             formulario.reject("Cliente não foi encontrado!");
+        } else {
+            Optional<Cliente> clienteSelecionado = clienteDAO.comId(Long.parseLong(cliente_id));
+
+            if (clienteSelecionado.isPresent()) {
+                Cliente cliente = clienteSelecionado.get();
+                projeto.setCliente(cliente);
+            }
         }
-        else {
-            Optional<Cliente> cliente = clienteDAO.comId(Long.parseLong(cliente_id));
-        
-            if (cliente.isPresent()) {
-                Cliente cliente_ = cliente.get();
-                projeto.setCliente(cliente_);
-            } 
-        }            
 
         projeto.save();
-        //return redirect(routes.TarefaController.formularioDeNovaTarefa());
-        return ok();
-    } 
+        flash("success", "Gravado com sucesso!");
+        return redirect(routes.TarefaController.formularioDeNovaTarefa());
+    }
 }
